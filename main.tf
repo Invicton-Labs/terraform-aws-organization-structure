9be0d8fd5c2923adb05e4d791684d28cfd973a20
KyleKotowick <kotowick@invictonlabs.com>
2022-06-18 19:39:56 -0400

    Add support for accounts variable

--- main.tf
+++ main.tf
@@ -1,45 +1,38 @@
-module "dynamic_depends_on" {
-  source             = "./dynamic-depends-on"
-  dynamic_depends_on = var.dynamic_depends_on
-}
-
-data "aws_organizations_organization" "organization" {
-  depends_on = [
-    module.dynamic_depends_on
-  ]
-}
+data "aws_organizations_organization" "organization" {}
 
 locals {
-  accounts_by_id = data.aws_organizations_organization.organization.accounts == null ? null : {
-    for account in data.aws_organizations_organization.organization.accounts :
-    account.id => account
-  }
-
-  accounts_by_name = data.aws_organizations_organization.organization.accounts == null ? null : {
-    for account in data.aws_organizations_organization.organization.accounts :
-    account.name => account
-  }
-
-  accounts_by_arn = data.aws_organizations_organization.organization.accounts == null ? null : {
-    for account in data.aws_organizations_organization.organization.accounts :
-    account.arn => account
-  }
-
-  accounts_by_root_email = data.aws_organizations_organization.organization.accounts == null ? null : {
-    for account in data.aws_organizations_organization.organization.accounts :
-    account.email => account
+  org_entity = {
+    id                   = data.aws_organizations_organization.organization.id
+    name                 = "Organization"
+    arn                  = data.aws_organizations_organization.organization.arn
+    parent_id            = null
+    ancestor_entity_ids  = []
+    parent_arn           = null
+    ancestor_entity_arns = []
+    org_path             = data.aws_organizations_organization.organization.id
   }
 
   roots = {
     for root in data.aws_organizations_organization.organization.roots :
     root.id => {
-      id                 = root.id
-      name               = root.name
-      arn                = root.arn
-      parent_id          = null
-      ancestor_ids       = []
-      principal_org_path = "${data.aws_organizations_organization.organization.id}/${root.id}"
+      id        = root.id
+      name      = root.name
+      arn       = root.arn
+      parent_id = local.org_entity.id
+      ancestor_entity_ids = concat([
+        local.org_entity.id
+      ], local.org_entity.ancestor_entity_ids)
+      parent_arn = local.org_entity.arn
+      ancestor_entity_arns = concat([
+        local.org_entity.arn
+      ], local.org_entity.ancestor_entity_arns)
+      org_path = "${local.org_entity.org_path}/${root.id}"
     }
+  }
+
+  all_accounts_by_id = data.aws_organizations_organization.organization.accounts == null ? null : {
+    for account in data.aws_organizations_organization.organization.accounts :
+    account.id => account
   }
 }
 
@@ -97,84 +90,125 @@ data "aws_organizations_organizational_units" "level_5" {
 
 locals {
   level_1_ous = merge([
-    for k, v in data.aws_organizations_organizational_units.level_1 :
+    for ou in values(data.aws_organizations_organizational_units.level_1) :
     {
-      for child in v.children :
+      for child in ou.children :
       child.id => merge(child, {
-        parent_id = v.parent_id
-        ancestor_ids = [
-          v.parent_id
-        ]
-        principal_org_path = "${local.roots[k].principal_org_path}/${child.id}"
+        parent_id  = ou.parent_id
+        parent_arn = local.roots[ou.parent_id].arn
+        ancestor_entity_ids = concat(
+          [
+            ou.parent_id
+          ],
+          local.roots[ou.parent_id].ancestor_entity_ids
+        )
+        ancestor_entity_arns = concat(
+          [
+            local.roots[ou.parent_id].arn
+          ],
+          local.roots[ou.parent_id].ancestor_entity_arns
+        )
+        org_path = "${local.roots[ou.parent_id].org_path}/${child.id}"
       })
     }
   ]...)
   level_2_ous = merge([
-    for k, v in data.aws_organizations_organizational_units.level_2 :
+    for ou in values(data.aws_organizations_organizational_units.level_2) :
     {
-      for child in v.children :
+      for child in ou.children :
       child.id => merge(child, {
-        parent_id = v.parent_id
-        ancestor_ids = concat(
+        parent_id  = ou.parent_id
+        parent_arn = local.level_1_ous[ou.parent_id].arn
+        ancestor_entity_ids = concat(
           [
-            v.parent_id
+            ou.parent_id
           ],
-          local.level_1_ous[v.parent_id].ancestor_ids
+          local.level_1_ous[ou.parent_id].ancestor_entity_ids
         )
-        principal_org_path = "${local.level_1_ous[v.parent_id].principal_org_path}/${child.id}"
+        ancestor_entity_arns = concat(
+          [
+            local.level_1_ous[ou.parent_id].arn
+          ],
+          local.level_1_ous[ou.parent_id].ancestor_entity_arns
+        )
+        org_path = "${local.level_1_ous[ou.parent_id].org_path}/${child.id}"
       })
     }
   ]...)
   level_3_ous = merge([
-    for k, v in data.aws_organizations_organizational_units.level_3 :
+    for ou in values(data.aws_organizations_organizational_units.level_3) :
     {
-      for child in v.children :
+      for child in ou.children :
       child.id => merge(child, {
-        parent_id = v.parent_id
-        ancestor_ids = concat(
+        parent_id  = ou.parent_id
+        parent_arn = local.level_2_ous[ou.parent_id].arn
+        ancestor_entity_ids = concat(
           [
-            v.parent_id
+            ou.parent_id
           ],
-          local.level_2_ous[v.parent_id].ancestor_ids
+          local.level_2_ous[ou.parent_id].ancestor_entity_ids
         )
-        principal_org_path = "${local.level_2_ous[v.parent_id].principal_org_path}/${child.id}"
+        ancestor_entity_arns = concat(
+          [
+            local.level_2_ous[ou.parent_id].arn
+          ],
+          local.level_2_ous[ou.parent_id].ancestor_entity_arns
+        )
+        org_path = "${local.level_2_ous[ou.parent_id].org_path}/${child.id}"
       })
     }
   ]...)
   level_4_ous = merge([
-    for k, v in data.aws_organizations_organizational_units.level_4 :
+    for ou in values(data.aws_organizations_organizational_units.level_4) :
     {
-      for child in v.children :
+      for child in ou.children :
       child.id => merge(child, {
-        parent_id = v.parent_id
-        ancestor_ids = concat(
+        parent_id  = ou.parent_id
+        parent_arn = local.level_3_ous[ou.parent_id].arn
+        ancestor_entity_ids = concat(
           [
-            v.parent_id
+            ou.parent_id
           ],
-          local.level_3_ous[v.parent_id].ancestor_ids
+          local.level_3_ous[ou.parent_id].ancestor_entity_ids
         )
-        principal_org_path = "${local.level_3_ous[v.parent_id].principal_org_path}/${child.id}"
+        ancestor_entity_arns = concat(
+          [
+            local.level_3_ous[ou.parent_id].arn
+          ],
+          local.level_3_ous[ou.parent_id].ancestor_entity_arns
+        )
+        org_path = "${local.level_3_ous[ou.parent_id].org_path}/${child.id}"
       })
     }
   ]...)
   level_5_ous = merge([
-    for k, v in data.aws_organizations_organizational_units.level_5 :
+    for ou in values(data.aws_organizations_organizational_units.level_5) :
     {
-      for child in v.children :
+      for child in ou.children :
       child.id => merge(child, {
-        parent_id = v.parent_id
-        ancestor_ids = concat(
+        parent_id  = ou.parent_id
+        parent_arn = local.level_4_ous[ou.parent_id].arn
+        ancestor_entity_ids = concat(
           [
-            v.parent_id
+            ou.parent_id
           ],
-          local.level_4_ous[v.parent_id].ancestor_ids
+          local.level_4_ous[ou.parent_id].ancestor_entity_ids
         )
-        principal_org_path = "${local.level_4_ous[v.parent_id].principal_org_path}/${child.id}"
+        ancestor_entity_arns = concat(
+          [
+            local.level_4_ous[ou.parent_id].arn
+          ],
+          local.level_4_ous[ou.parent_id].ancestor_entity_arns
+        )
+        org_path = "${local.level_4_ous[ou.parent_id].org_path}/${child.id}"
       })
     }
   ]...)
 
   all_ous = merge(
+    {
+      (local.org_entity.id) = local.org_entity
+    },
     local.roots,
     local.level_1_ous,
     local.level_2_ous,
@@ -183,61 +217,104 @@ locals {
     local.level_5_ous,
   )
 
-  all_ous_with_children = {
+  // Add metadata to each OU, for its ancestor and descendant OUs
+  all_ous_with_family = {
     for ou_id, ou in local.all_ous :
-    ou_id => merge(ou, {
-      children_ids = [
-        for child_ou_id, child_ou in local.all_ous :
-        child_ou_id
-        if child_ou.parent_id == ou_id
-      ]
-      descendant_ids = [
-        for descendant_ou_id, descendant_ou in local.all_ous :
-        descendant_ou_id
-        if contains(descendant_ou.ancestor_ids, ou_id)
-      ]
-      // Add a field that has all ancestor OU IDs and this OU's own ID. These are the OU IDs that apply to anything in this OU.
-      applicable_ou_ids = concat([ou_id], ou.ancestor_ids)
+    ou_id => merge(ou,
+      {
+
+        // Add a field that has all ancestor OU IDs and this OU's own ID. These are the OU IDs that apply to anything in this OU.
+        applicable_entity_ids  = concat([ou_id], ou.ancestor_entity_ids)
+        applicable_entity_arns = concat([ou.arn], ou.ancestor_entity_arns)
+
+        // Add IDs and ARNs of direct children
+        child_ou_ids = [
+          for child_ou_id, child_ou in local.all_ous :
+          child_ou_id
+          if child_ou.parent_id == ou_id
+        ]
+        child_ou_arns = [
+          for child_ou in values(local.all_ous) :
+          child_ou.arn
+          if child_ou.parent_id == ou_id
+        ]
+
+        // Add IDs and ARNs of all descendants
+        descendant_ou_ids = [
+          for descendant_ou_id, descendant_ou in local.all_ous :
+          descendant_ou_id
+          if contains(descendant_ou.ancestor_entity_ids, ou_id)
+        ]
+        descendant_ou_arns = [
+          for descendant_ou in values(local.all_ous) :
+          descendant_ou.arn
+          if contains(descendant_ou.ancestor_entity_ids, ou_id)
+        ]
     })
   }
 
-  all_ous_with_arns = {
-    for ou_id, ou in local.all_ous_with_children :
-    ou_id => merge(ou, {
-      // If it's an organization root, then the parent ARN is the organization ARN
-      // Otherwise, it's the ARN of the parent OU or root
-      parent_arn = ou.parent_id == null ? data.aws_organizations_organization.organization.arn : local.all_ous[ou.parent_id].arn
-      ancestor_arns = concat([
-        for ancestor_id in ou.ancestor_ids :
-        local.all_ous[ancestor_id].arn
-        ], [
-        data.aws_organizations_organization.organization.arn
-        // Add the organization ARN to the list
-      ])
-      applicable_ou_arns = concat([
-        for applicable_ou_id in ou.applicable_ou_ids :
-        local.all_ous[applicable_ou_id].arn
-        ], [
-        // Add the organization ARN to the list
-        data.aws_organizations_organization.organization.arn
-      ])
-      children_arns = [
-        for child_id in ou.children_ids :
-        local.all_ous[child_id].arn
+  // If account parent data was provided, use it to add account family data
+  all_ous_by_id = {
+    for ou_id, ou in local.all_ous_with_family :
+    ou_id => merge(ou, local.var_account_parent_ids != null ? {
+      // A list of all account IDs that are directly within this OU
+      child_account_ids = [
+        for account_id, parent_id in local.var_account_parent_ids :
+        account_id
+        if parent_id == ou_id
       ]
-      descendant_arns = [
-        for descendant_id in ou.descendant_ids :
-        local.all_ous[descendant_id].arn
+      child_account_arns = [
+        for account_id, parent_id in local.var_account_parent_ids :
+        local.all_accounts_by_id[account_id].arn
+        if parent_id == ou_id
       ]
-      // Add the organization ID to the list
-      ancestor_ids = concat(ou.ancestor_ids, [data.aws_organizations_organization.organization.id])
-      // Add the organization ID to the list
-      applicable_ou_ids = concat(ou.applicable_ou_ids, [data.aws_organizations_organization.organization.id])
-    })
+      // A list of all account IDs that are directly within this OU or any of its descendant OUs
+      descendant_account_ids = [
+        for account_id, parent_id in local.var_account_parent_ids :
+        account_id
+        if contains(concat([ou_id], ou.descendant_ou_ids), parent_id)
+      ]
+      descendant_account_arns = [
+        for account_id, parent_id in local.var_account_parent_ids :
+        local.all_accounts_by_id[account_id].arn
+        if contains(concat([ou_id], ou.descendant_ou_ids), parent_id)
+      ]
+    } : {})
   }
 
   all_ous_by_arn = {
-    for ou in values(local.all_ous_with_arns) :
+    for ou in values(local.all_ous_by_id) :
     ou.arn => ou
+  }
+
+  all_ous_by_name = {
+    for ou in values(local.all_ous_by_id) :
+    ou.name => ou
+  }
+
+  accounts_by_id = local.all_accounts_by_id == null ? null : {
+    for account in values(local.all_accounts_by_id) :
+    account.id => merge(account,
+      local.var_account_parent_ids != null ? contains(keys(local.var_account_parent_ids), account.id) ? {
+        ou = local.all_ous_by_id[local.var_account_parent_ids[account.id]]
+      } : {} : {}
+    )
+    // Only include accounts that are active, unless we want to get non-active accounts as well
+    if !local.var_active_accounts_only || account.status == "ACTIVE"
+  }
+
+  accounts_by_name = local.accounts_by_id == null ? null : {
+    for account in values(local.accounts_by_id) :
+    account.name => account
+  }
+
+  accounts_by_arn = local.accounts_by_id == null ? null : {
+    for account in values(local.accounts_by_id) :
+    account.arn => account
+  }
+
+  accounts_by_root_email = local.accounts_by_id == null ? null : {
+    for account in values(local.accounts_by_id) :
+    account.email => account
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,17 @@
-variable "dynamic_depends_on" {
-    description = "Used in the same manner as the Terraform `depends_on` variable, except that it can handle dynamic values such as locals."
-    type = any
-    default = null
+variable "account_parent_ids" {
+  description = "A map of account ID to parent ID. This is optional; if it is provided, account IDs will be added to each OU (the accounts that are within that OU)."
+  type        = map(string)
+  default     = null
+}
+locals {
+  var_account_parent_ids = var.account_parent_ids
+}
+
+variable "active_accounts_only" {
+  description = "Whether the listed accounts should only include accounts that are in active status. If `true`, accounts in \"SUSPENDED\" or \"PENDING_CLOSURE\" status will not be returned."
+  type        = bool
+  default     = true
+}
+locals {
+  var_active_accounts_only = var.active_accounts_only != null ? var.active_accounts_only : true
 }
